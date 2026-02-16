265c03ba6a61921644b3ccebc954b768246c3fac
devdilyorbek <dilyorbekdev@gmail.com>
2026-02-16 14:28:41 +0500

    navbar & navbaronScroll

--- src/components/site/button.tsx
+++ src/components/site/button.tsx
@@ -1,15 +1,15 @@
-
-function Button({ text, btntype }: {
+function Button({ text, btntype,className }: {
     text: string;
-    btntype: "primary" | "secondary"
+    btntype: "primary" | "secondary", 
+    className?: string
 }) {
     return (
         <>
 
             {btntype === "primary" ? (
-                <button className="bg-primary text-[16px] text-secondary px-[26px] py-[14.5px] rounded-md">{text}</button>
+                <button className={`bg-primary text-[16px] text-secondary px-[26px] py-[14.5px] rounded-[15px] ${className}`}>{text}</button>
             ) : (
-                <button className="bg-secondary border primary-border  text-[16px] text-primary px-[26px] py-[14.5px] rounded-md">{text}</button>
+                <button className={`bg-secondary border primary-border  text-[16px] text-primary px-[26px] py-[14.5px] rounded-[15px] ${className}`}>{text}</button>
             )}
         </>
     )
